bae6a07bf924aa0c704ee520a5eb07fe04ebe4b4
Sava Lazic <savalazic9@gmail.com>
2018-11-26 22:07:16 +0100

    setup rollup bundler

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import replace from "rollup-plugin-replace";
+import typescript from "rollup-plugin-typescript";
+
+const dev = "development";
+const prod = "production";
+const env =
+  process.env.NODE_ENV === prod || process.env.NODE_ENV === dev
+    ? process.env.NODE_ENV
+    : dev;
+
+const config = {
+  input: "src/index.ts",
+  output: {
+    file: "dist/index.js",
+    format: "cjs",
+    globals: {
+      react: "React",
+      "react-dom": "ReactDOM",
+      "styled-components": "styled"
+    }
+  },
+  plugins: [
+    replace({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+    }),
+    resolve(),
+    babel({
+      exclude: "node_modules/**", // only transpile our source code
+      externalHelpers: true
+    }),
+    typescript()
+  ],
+  external: ["react", "react-dom", "styled-components"]
+};
+
+export default config;
---
+++ scripts/run-travis-tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ev
+npm run lint
+# npm run test:coverage
+# coveralls < ./coverage/lcov.info
