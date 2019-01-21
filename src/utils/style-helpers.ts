2aedf53ff95b119257637e149369a75e9b9d63ef
Sava Lazic <savalazic9@gmail.com>
2019-01-22 00:08:53 +0100

    add theme helpers

--- src/utils/style-helpers.ts
+++ src/utils/style-helpers.ts
@@ -1,12 +1,3 @@
-export const truncateStyles = (width: string) => {
-  return `
-    width: ${width};
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  `;
-};
-
 export const setDimensions = (width: string, height: string) => {
   return `
     width: ${width};
